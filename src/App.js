31c5594e13f753a079208d66079c33fe40f6e3ef
Treasure Kabareebe <treasurecalm@gmail.com>
2022-06-28 04:10:50 +0300

    Add the TaskForm component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import AddTaskForm from "./components/AddTaskForm";
 
 function App() {
-  return <div className="App">
-    <Header />
-  </div>;
+  return (
+    <div className="App">
+      <Header />
+      <AddTaskForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/AddTaskForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddTaskForm = () => {
+  return (
+    <form>
+      <input type="checkbox"/>
+      <input type="text" placeholder="Create a new todo..." />
+    </form>
+  );
+};
+
+export default AddTaskForm;
