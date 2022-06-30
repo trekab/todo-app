a53a819c74bddf853b955d54075f2a6f6c0d1cb4
Treasure Kabareebe <treasurecalm@gmail.com>
2022-06-30 06:30:59 +0300

    Add filter component and style them for small screens

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,17 +1,24 @@
 import React from "react";
+import Filters from "./Filters";
 import Task from "./Task";
 
 const TaskList = ({ tasks }) => {
   return (
-    <ul className="task-list">
-      {tasks.map((task, i) => (
-        <Task task={task} key={i} />
-      ))}
-      <li className="task-list__footer">
-        <div>5 items left</div>
-        <div>Clear Completed</div>
-      </li>
-    </ul>
+    <>
+      <ul className="task-list">
+        {tasks.map((task, i) => (
+          <Task task={task} key={i} />
+        ))}
+        <li className="task-list__footer">
+          <div className="task-list__footer--item">5 items left</div>
+          {/* <Filters /> */}
+          <div className="task-list__footer--item">Clear Completed</div>
+        </li>
+      </ul>
+      <div className="filters">
+        <Filters />
+      </div>
+    </>
   );
 };
 
